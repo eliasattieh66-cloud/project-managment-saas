13d65e5d4074fa8ede86880ab6790c5387a89c9c
eliasattieh <eliasattieh66@gmail.com>
2026-08-08 14:48:12 +0300

    Centralize backend environment configuration

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,7 @@
 import "dotenv/config";
 import app from "./app.js";
+import { env } from "./config/env.js";
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(env.port, () => {
+  console.log(`Server is running on port ${env.port}`);
 });
